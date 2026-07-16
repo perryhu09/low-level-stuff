006101cf8c1e3ac59f186d5d8f700bb8a82dbda5
dom <dominichx001@gmail.com>
2026-07-16 18:57:11 -0400

    asm

--- c/course/intro/name.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char name[32]; 
-    printf("Hello! What is your name?\n"); 
-    scanf("%s", &name);
-    printf("Hello %s!\n", name);
-    return 0;
-}
--- x86-asm/assembly-school/day3/c-fn/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-extern int test(int,int);
-
-int test(int a, int b) {
-    printf("HERE!\n");
-    return a + b;
-}
